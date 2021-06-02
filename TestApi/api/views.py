9bb454ac9bea1dd0f655f7d3ad32e4c08afa5692
mrlink01 <linkm2@labcorp.com>
2021-06-02 16:26:00 -0400

    Changed API response to JSON data

--- TestApi/api/views.py
+++ TestApi/api/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 #from .templates import api
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return JsonResponse({
+        "Test":"Data"
+    })
